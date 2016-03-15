f92356001a3cc678ca6c75bad543241b11fe916d
yl <ylyan126@126.com>
2016-03-15 18:50:46 +0800

    描述：

---
+++ src/main/java/com/yy/controller/Test.java
@@ -0,0 +1,16 @@
+package com.yy.controller;
+
+/**
+ * Created by yl on 2016/3/15.
+ */
+public class Test {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = [" + args + "]");
+
+    }
+
+}
